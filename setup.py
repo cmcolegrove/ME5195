04bd63e8fec6af9f41bb37de810eadf89253cecc
Charlie Colegrove <charles-colegrove@uiowa.edu>
2026-02-09 19:41:47 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'hw1_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cmcolegrove',
+    maintainer_email='cmcolegrove@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'battery_monitor = hw1_pkg.battery_monitor:main'
+        ],
+    },
+)
